c892be82683f6a5b9c8f02215b4e6967f3ee4110
Reza Nikfal <mr.nikfal@gmail.com>
2024-01-08 20:17:02 -0600

    Move routes to router.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,5 @@
-import {
-  Route,
-  RouterProvider,
-  createBrowserRouter,
-  createRoutesFromElements,
-} from "react-router-dom";
-import RootLayout from "./pages/RootLayout";
-import Home from "./pages/Home";
-import About from "./pages/About";
-import Contact from "./pages/Contact";
-import Help from "./pages/Help";
-import PageNotFound from "./pages/PageNotFound";
-import Careers from "./pages/Careers";
-import CareersLayout from "./pages/CareersLayout";
-import { careerApi, careerApiById } from "./services/careerService";
-import CareersDetail from "./pages/CareersDetail";
-import CareersError from "./pages/CareersError";
-
-const router = createBrowserRouter(
-  createRoutesFromElements(
-    <Route path="/" element={<RootLayout />}>
-      <Route index element={<Home />} />
-      <Route path="about" element={<About />}>
-        <Route path="contact" element={<Contact />} />
-        <Route path="help" element={<Help />} />
-      </Route>
-      <Route path="careers" element={<CareersLayout />}>
-        <Route index element={<Careers />} loader={careerApi} />
-        <Route
-          path=":id"
-          element={<CareersDetail />}
-          loader={careerApiById}
-          errorElement={<CareersError />}
-        />
-      </Route>
-      <Route path="*" element={<PageNotFound />} />
-    </Route>
-  )
-);
+import { RouterProvider } from "react-router-dom";
+import router from "./services/router";
 
 function App() {
   return <RouterProvider router={router} />;
